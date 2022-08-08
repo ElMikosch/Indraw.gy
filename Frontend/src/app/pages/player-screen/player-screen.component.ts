66a926ecc1344e3ef6a86ebd26778a4b1f95b1ff
Nico Struchholtz <Nico.Struchholtz@Inlogy.de>
2022-08-08 14:15:41 +0200

    commit

--- Frontend/src/app/pages/player-screen/player-screen.component.ts
+++ Frontend/src/app/pages/player-screen/player-screen.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { IndrawgyHubService } from 'src/app/hub/indrawgy-hub.service';
+import { GameStatus } from 'src/app/models/game-status';
 import { PlayerScreenFacade } from './player-screen.facade';
 
 @Component({
@@ -12,10 +14,16 @@ import { PlayerScreenFacade } from './player-screen.facade';
 })
 export class PlayerScreenComponent implements OnInit {
   playerReady = false;
+  GameStatus = GameStatus;
 
-  constructor(private facade: PlayerScreenFacade) {}
+  constructor(
+    private facade: PlayerScreenFacade,
+    public indrawgyHub: IndrawgyHubService
+  ) {}
 
-  async ngOnInit(): Promise<void> {}
+  async ngOnInit(): Promise<void> {
+    await this.indrawgyHub.registerPlayer();
+  }
 
   changePlayerReadyState(): void {
     this.playerReady = !this.playerReady;
